635686b5d980f7eb244dbdbdb8dc8b1c7426ef28
miaojianqiang <nil.miu@kucoin.com>
2019-07-27 11:17:25 +0800

    update

--- superlists/deploy_tools/fabfile.py
+++ superlists/deploy_tools/fabfile.py
@@ -1,2 +1,52 @@
 # -*- coding: utf-8 -*-
 # Created by nil_mmm
+
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+import random
+
+REPO_URL = "https://github.com/nilmiao/superlists.git"
+
+
+def deploy():
+    site_folder = f'/root/sites/superlists.miaogodthink.top'
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_latest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_static_files(source_folder)
+
+
+def _create_directory_structure_if_necessary(site_folder):
+    for subfolder in ('source'):
+        run(f'mkdir -p {site_folder}/{subfolder}')
+
+
+def _get_latest_source(source_folder):
+    if exists(source_folder + '/.git'):
+        run(f'cd {source_folder} && git fetch')
+    else:
+        run(f'git clone {REPO_URL} {source_folder}')
+    current_commit = local('git log -n 1 --format=%H', caputure=True)
+    run(f'cd {source_folder} && git reset --hard {current_commit}')
+
+
+def _update_settings(source_folder,site_name):
+    settings_path = source_folder + '/superlists/setting.py'
+    sed(settings_path,"DEBUG=True","DEBUG=False")
+    sed(settings_path,
+        'ALLOWED_HOSTS = .+$',
+        f'ALLOWED_HSOTS = ["{site_name}"]'
+        )
+    secret_key_file = source_folder+'/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file,f'SECRET_KEY="{key}')
+    append(settings_path,'\nfrom .secret_key import SECRET_KEY')
+
+def _update_static_files(source_folder):
+    run(
+        f'cd {source_folder}'
+        ' && python manage.py collectstatic --noinput'
+    )
